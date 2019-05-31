f2fb44420a8e1b83c3af3089b65b708676eff37a
Seint <seint.nandar.oo@digitallaboratory.net>
2019-05-31 15:39:29 +0630

    #card-22109518 - example11-3

--- Chapter11/example11-3.php
+++ Chapter11/example11-3.php
@@ -25,10 +25,10 @@ try {
 	// commit the transaction
 	$conn->commit();
 	echo 'New records created successfully';
-	} catch ( PDOException $e ) {
-		// roll back the transaction if something failed
-		$conn->rollback();
-		echo 'Error: ' . $e->getMessage();
-	}
+} catch ( PDOException $e ) {
+	// roll back the transaction if something failed
+	$conn->rollback();
+	echo 'Error: ' . $e->getMessage();
+}
 $conn = null;
 ?>
