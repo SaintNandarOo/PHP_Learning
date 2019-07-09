c3b1b7b4f69782981911347538b857f25a318cf8
Seint <seint.nandar.oo@digitallaboratory.net>
2019-07-09 12:39:21 +0630

    #card-23724456 - Chapter 12 (after review)

--- Chapter12/example6.php
+++ Chapter12/example6.php
@@ -1,7 +1,7 @@
 <?php
 $s = simplexml_load_file( __DIR__ . '/address-book.xml' );
 $people = $s->xpath( '/address-book/person' );
-foreach( $people as $p ) {
+foreach ( $people as $p ) {
 	list( $firstname ) = $p->xpath( 'firstname' );
 	list( $lastname ) = $p->xpath( 'lastname' );
 	print $firstname . $lastname . '<br>';
