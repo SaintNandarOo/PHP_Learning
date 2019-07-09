54e8555b35fd6df7133fdf5028ffc53cd2e07873
Seint <seint.nandar.oo@digitallaboratory.net>
2019-07-09 09:29:09 +0630

    #card-23724456 - Chapter 12

--- Chapter12/example4.php
+++ Chapter12/example4.php
@@ -4,7 +4,7 @@ $reader->open( __DIR__ . '/book.xml' );
 /* Loop through document */
 while ( $reader->read() ) {
 	/* If you're at an element named 'author' */ 
-	if( $reader->nodeType == XMLREADER::ELEMENT && $reader->localName == 'author' ) {
+	if ( $reader->nodeType == XMLREADER::ELEMENT && $reader->localName == 'author' ) {
 		/* Move to the text node and print it out */
 		$reader->read();
 		print $reader->value . '<br>';
--- Chapter12/example5.php
+++ Chapter12/example5.php
@@ -6,7 +6,7 @@ $reader->open( 'book.xml' );
 /* Loop through document */
 while ( $reader->read () ) {
 	/* If you're at an element named 'author' */
-	if( $reader->nodeType == XMLREADER::ELEMENT && $reader->localName == 'book' ) {
+	if ( $reader->nodeType == XMLREADER::ELEMENT && $reader->localName == 'book' ) {
 		/* Process author element */
 		$reader->moveToAttribute( 'isbn' );
 		print $reader->value . '<br>';
